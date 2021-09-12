29c868ecfbcb05781cd68148dd42ca9d7630e264
Qamar <qalkhatib0@gmail.com>
2021-09-12 17:22:10 +0300

    lab 1 solved

---
+++ src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component{
+
+    render(){
+        return(
+            <h4>Author: Qamar Alkhatib</h4>
+        )
+
+        
+    }
+}
+
+export default Footer;
---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './css/Header.css';
+
+class Header extends React.Component{
+
+    render(){
+        return(
+            <div>
+            <h1>Horned Beasts</h1>
+            
+            </div>
+        )
+    }
+}
+
+export default Header;
